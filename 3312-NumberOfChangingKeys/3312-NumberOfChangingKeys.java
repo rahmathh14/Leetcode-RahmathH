2c43818d1d6552c4d8890c7172362f2e2760d8cc
rahmathh14 <rahmathh1405@gmail.com>
2026-08-11 14:19:18 +0530

    Create 3312-NumberOfChangingKeys

---
+++ 3312-NumberOfChangingKeys/3312-NumberOfChangingKeys.java
@@ -0,0 +1,19 @@
+// Last updated: 8/11/2026, 2:19:18 PM
+class Solution {
+
+    public int countKeyChanges(String s) {
+
+        int count = 0;
+
+        s = s.toLowerCase();
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
