e6bf100da023d59b5c5f7e7f53badb00475f4e68
rahmathh14 <rahmathh1405@gmail.com>
2026-08-11 14:19:56 +0530

    Create 2723-FindTheLongestBalancedSubstringOfABinaryString

---
+++ 2723-FindTheLongestBalancedSubstringOfABinaryString/2723-FindTheLongestBalancedSubstringOfABinaryString.java
@@ -0,0 +1,13 @@
+// Last updated: 8/11/2026, 2:19:56 PM
+class Solution {
+    public int findTheLongestBalancedSubstring(String s) {
+        int res = 0;
+        String temp = "01";
+        while(temp.length() <= s.length()){
+            if(s.contains(temp))
+                res = temp.length();
+            temp = "0" + temp + "1";
+        }
+        return res;
+    }
+}
